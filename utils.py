04c68cdb3a24e4919e029991a48e72ee99ae027a
K Mohith Goud <128036418+Mohith202@users.noreply.github.com>
2024-06-17 20:16:14 +0530

    updated

---
+++ utils.py
@@ -0,0 +1,35 @@
+import os
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+def load_pdfs_from_file(file_path):
+    loader = PyPDFLoader(file_path)
+    documents = loader.load()
+    return documents
+
+def save_uploaded_file(uploaded_file, directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    file_path = os.path.join(directory, uploaded_file.name)
+    with open(file_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+    return file_path
+      
+def load_pdfs_from_folder(folder_path):
+    documents = []
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".pdf"):
+            file_path = os.path.join(folder_path, filename)
+            loader = PyPDFLoader(file_path)
+            docs = loader.load()
+            for i, doc in enumerate(docs):
+                doc.metadata['title'] = filename
+                doc.metadata['page'] = i + 1
+            documents.extend(loader.load())
+    return documents
+
+def split_docs(documents, chunk_size=500, chunk_overlap=10):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+    docs = text_splitter.split_documents(documents)
+    return docs
